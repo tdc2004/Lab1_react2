1aaa07683bbcc5aa8310d7779a5f7641d5e29c4b
chinhdev <chinhtdph40493@fpr.edu.vn>
2024-03-12 22:20:02 +0700

    'lab2_React2

--- App.tsx
+++ App.tsx
@@ -1,42 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
-import Bai1 from './Bai1'
-import Bai2 from './Bai2'
-import Bai3 from './Bai3'
+import React from 'react'
+import Main from './Lab2/Main'
 
 const App = () => {
-  const [text, setText] = useState('');
-  const [error, setError] = useState('');
-
-  const handleTextChange = (text) => {
-    setText(text);
-  };
-
-  const handleTextChangeError = (text) => {
-    if (!text) {
-      setError('Vui lòng nhập thông tin');
-    } else {
-      setError('');
-    }
-  };
-
   return (
-    <View style={{padding:10}}>
-      {/* <Bai1 iconLeft={require('./image/back-button.png')} iconRight={require('./image/man.png')} title={"Header"}/>
-      <Bai1 iconLeft={null} iconRight={require('./image/man.png')} title={"Title"}/>
-      <Bai1 iconLeft={null} iconRight={null} title={"Center"}/> */}
-      <Bai2/>
-      {/* <Bai3 label={"Title"}
-       onChangeText={handleTextChangeError}
-      placeholder={"Vui lòng nhập"}
-      errorMessage={error}/>
-      <Bai3 label={"Title"}
-       onChangeText={handleTextChange}
-      placeholder={"Vui lòng nhập"}
-      errorMessage={null}/>
-      <Text style={{marginLeft:10}}>Text nhập là: {text}</Text> */}
-
-    </View>
+    <Main />
   )
 }
 
